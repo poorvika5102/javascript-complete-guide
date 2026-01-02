70e13a8748240f2b520765194e32f945a2256233
Poorvika A C <acpoorvika@gmail.com>
2026-01-02 11:06:52 +0530

    Add JavaScript variables and data types examples

---
+++ 01-basics/01-variables-datatypes.js
@@ -0,0 +1,31 @@
+// Variables
+let userName = "Poorvika";
+const birthYear = 2003;
+
+// Data types
+let age = 22;                 // number
+let isStudent = true;         // boolean
+let city;                     // undefined
+let score = null;             // null
+let skills = ["HTML", "CSS", "JavaScript"]; // array
+let profile = {               // object
+  role: "Frontend Developer",
+  experience: "Beginner"
+};
+
+// Console output
+console.log("User Name:", userName);
+console.log("Birth Year:", birthYear);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("City:", city);
+console.log("Score:", score);
+console.log("Skills:", skills);
+console.log("Profile:", profile);
+
+// Type checking
+console.log(typeof userName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof score); // special case
