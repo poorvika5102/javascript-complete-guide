a94ef0e286fce4d5e3ed61f22c684746190a7119
Poorvika A C <acpoorvika@gmail.com>
2026-01-02 11:10:27 +0530

    Add JavaScript control flow examples with conditions and loops

---
+++ 02-control-flow/01-control-flow.js
@@ -0,0 +1,36 @@
+// Conditional statements
+let score = 85;
+
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 75) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C");
+}
+
+// Switch statement
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the work week");
+    break;
+  case "Friday":
+    console.log("End of the work week");
+    break;
+  default:
+    console.log("Midweek day");
+}
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+  console.log("For loop iteration:", i);
+}
+
+// While loop
+let count = 1;
+while (count <= 3) {
+  console.log("While loop count:", count);
+  count++;
+}
